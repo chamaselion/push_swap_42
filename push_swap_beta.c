14b8c7596485502447147ecba8eb2bb7b56b5b7f
chamaselion <chamaselion@gmail.com>
2024-05-06 18:14:10 +0200

    Trying to make it work and cleaning the code

--- push_swap_beta.c
+++ push_swap_beta.c
@@ -1,55 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-
-#define MAX_SIZE 100
-
-typedef struct {
-    int top;
-    int nbr[MAX_SIZE];
-    int current_a_smallest;
-    int current_a_biggest;
-    int full_len;
-    int full_move_count;
-    int* ar;
-    int current_mid1;
-    int current_mid2;
-    int current_mid3;
-    int current_mid4;
-} Stack;
-
-
-void ra(Stack* a);
-void pb(Stack* a, Stack* b);
-void rrb(Stack* a);
-void print_stack(Stack* a, char* name);
-int find_smallest(Stack* a);
-int find_biggest(Stack* a);
-void handle_a_to_be(Stack* a, Stack* b, int c);
-void let_it_be(Stack* a, Stack* b);
-void handle_a(Stack* a, Stack* b);
-void push_swap_beta(void);
-
-void sa(Stack *a)
-{
-    if (a->top >= 1)
-    {
-        int temp = a->nbr[a->top];
-        a->nbr[a->top] = a->nbr[a->top - 1];
-        a->nbr[a->top - 1] = temp;
-    }
-}
-
-void sb(Stack *b)
-{
-    if (b->top >= 1)
-    {
-        int temp = b->nbr[b->top];
-        b->nbr[b->top] = b->nbr[b->top - 1];
-        b->nbr[b->top - 1] = temp;
-    }
-}
+#include "push_swap.h"
 
 void print_array(int* array, int size)
 {
@@ -65,104 +14,6 @@ void print_array(int* array, int size)
         i++;
     }
     printf("]\n");
-}
-
-void ra(Stack* a)
-{
-    if (a->top < 0)
-    {
-        return;
-    }
-
-    int temp = a->nbr[a->top];
-
-    for (int i = a->top; i > 0; i--)
-    {
-        a->nbr[i] = a->nbr[i - 1];
-    }
-
-    a->nbr[0] = temp;
-    printf("ra\n");
-    a->full_move_count++;
-}
-
-void rb(Stack* b)
-{
-    if (b->top < 0)
-    {
-        return;
-    }
-
-    int temp = b->nbr[b->top];
-
-    for (int i = b->top; i > 0; i--)
-    {
-        b->nbr[i] = b->nbr[i - 1];
-    }
-
-    b->nbr[0] = temp;
-    printf("rb\n");
-    b->full_move_count++;
-}
-
-void pb(Stack* a, Stack* b)
-{
-    if (a->top >= 0)
-    {
-        b->nbr[++b->top] = a->nbr[a->top--];
-    }
-    printf("PUSHED HERE\n");
-    a->full_move_count++;
-}
-
-void pa(Stack* a, Stack* b)
-{
-    if (b->top >= 0)
-    {
-        a->nbr[++a->top] = b->nbr[b->top--];
-    }
-    printf("PUSHED HERE\n");
-    a->full_move_count++;
-}
-
-void rra(Stack* a)
-{
-    if (a->top < 0)
-    {
-        return;
-    }
-
-    int temp = a->nbr[0];
-
-
-    for (int i = 0; i < a->top; i++)
-    {
-        a->nbr[i] = a->nbr[i + 1];
-    }
-
-    a->nbr[a->top] = temp;
-    printf("rra\n");
-    a->full_move_count++;
-}
-
-void rrb(Stack* b)
-{
-    if (b->top < 0)
-    {
-        return;
-    }
-
-    int temp = b->nbr[0];
-
-
-    for (int i = 0; i < b->top; i++)
-    {
-        b->nbr[i] = b->nbr[i + 1];
-    }
-
-    b->nbr[b->top] = temp;
-    printf("rrb\n");
-    b->full_move_count++;
 }
 
 int* stack_to_array(Stack* a)
@@ -189,31 +40,6 @@ int check_phase(Stack* a, int cc)
         return(a->current_mid4);
 }
 
-void smart_rotate(Stack* a, Stack* b, int c)
-{
-    int move_count;
-
-    move_count = 0;
-
-        if(c >= (a->full_len / 2))
-        {
-            while (move_count < c)
-            {
-                rra(a);
-                move_count++;
-            }
-            move_count = 0;
-        }
-        if (c < (a->full_len / 2) )
-        {
-            while (move_count < c)
-            {
-                ra(a);
-                move_count++;
-            }
-    }
-}
-
 void sort_array(int* array, int size)
 {
     int i = 0;
@@ -232,77 +58,6 @@ void sort_array(int* array, int size)
         }
         i++;
     }
-}
-
-void print_stack(Stack* a, char* name)
-{
-    printf("%s: ", name);
-    for (int i = a->top; i >= 0; i--)
-    {
-        printf("%d ", a->nbr[i]);
-    }
-    printf("\n");
-}
-
-int find_smallest(Stack* a)
-{
-    int smallest = a->nbr[0];
-    int i;
-
-    i = 1;
-    while (i <= a->top)
-    {
-        if (a->nbr[i] < smallest)
-        {
-            smallest = a->nbr[i];
-        }
-        i++;
-    }
-    return smallest;
-}
-
-int find_smallest_b(Stack* b)
-{
-    int smallest = b->nbr[0];
-    int i;
-
-    i = 1;
-    while (i <= b->top)
-    {
-        if (b->nbr[i] < smallest)
-        {
-            smallest = b->nbr[i];
-        }
-    i++;
-    }
-    return smallest;
-}
-
-int find_biggest(Stack* a)
-{
-    int biggest = a->nbr[0];
-    int i;
-
-    i = 1;
-    while (i <= a->top)
-    {
-        if (a->nbr[i] > biggest)
-        {
-            biggest = a->nbr[i];
-        }
-        i++;
-    }
-    return biggest;
-}
-
-int count_len(Stack* a)
-{
-    return a->top + 1;
-}
-
-int count_len_b(Stack* b)
-{
-    return b->top + 1;
 }
 
 void mini_sort(Stack *a)
@@ -367,25 +122,26 @@ int is_sorted(Stack *a)
 
 void find_key_array(Stack* a)
 {
+    printf("%d\n", 1);
     int chunk_size;
     int chunk1_start;
     int chunk2_start;
     int chunk3_start;
     int chunk4_start;
-
+     
     if(a->top < 4)
         return ;
-
     chunk_size = a->top / 4;
     chunk1_start = 0;
     chunk2_start = chunk_size;
     chunk3_start = chunk_size * 2;
     chunk4_start  = a->top;
-
+    printf("%i, %i, %i, %i\n", chunk1_start, chunk2_start, chunk3_start, chunk4_start);
     a->current_mid1 = a->ar[chunk1_start];
     a->current_mid2 = a->ar[chunk2_start];
     a->current_mid3 = a->ar[chunk3_start];
     a->current_mid4 = a->ar[chunk4_start];
+    printf("%i, %i, %i, %i", a->current_mid1, a->current_mid2, a->current_mid3, a->current_mid4);
 }
 void find_value_a(Stack* a, Stack* b)
 {
@@ -431,8 +187,17 @@ void push_swap_beta(void)
     Stack b = {-1, {}, 0, 0, 0};
 
     fill_stack_a(&a);
-    print_stack(&a, "Stack a:\n");
-    print_stack(&b, "Stack b:\n");
+   
+      print_stack(&a, "Stack a:\n");
+   
+      print_stack(&b, "Stack b:\n");
+    stack_to_array(&a);
+      printf("PLS %s\n", "work");
+      print_array(a.ar, a.top - 1);
+   
+    sort_array(a.ar, a.top - 1);
+   
+      print_array(a.ar, a.top - 1);
 
     find_value_a(&a, &b);
 
