ea35dc8cb5a3c362f7cc02c0e8cd053f90b89251
chamaselion <chamaselion@gmail.com>
2024-05-07 23:44:51 +0200

    fixed for every case ond optimized a bit

--- push_swap_beta.c
+++ push_swap_beta.c
@@ -119,7 +119,7 @@ int is_sorted(Stack *a)
     int i = a->top;
     while (i > 0)
     {
-        if (a->nbr[i] < a->nbr[i - 1])
+        if (a->nbr[i] > a->nbr[i - 1])
         {
             return 0; // return 0 (false) if a pair of elements is out of order
         }
@@ -128,81 +128,19 @@ int is_sorted(Stack *a)
     return 1; // return 1 (true) if all elements are in order
 }
 
-void find_key_array(Stack* a)
+void bubble_sort_pass(Stack *a)
 {
-    int chunk_size;
-    int chunk1_end;
-    int chunk2_end;
-    int chunk3_end;
-    int chunk4_end;
-     
-    chunk_size = (a->top + 1) / 4;
-    chunk1_end = chunk_size;
-    chunk2_end = chunk_size * 2;
-    chunk3_end = chunk_size * 3;
-    chunk4_end  = a->top;
-    a->current_mid1 = a->ar[chunk1_end];
-    a->current_mid2 = a->ar[chunk2_end];
-    a->current_mid3 = a->ar[chunk3_end];
-    a->current_mid4 = a->ar[chunk4_end];
-
-}
-
-void find_value_a(Stack* a, Stack* b)
-{
-    int c;
-    int cc;
-    int i;
-    int key_storage;
-    int l;
-
-    cc = 1;
-    l = a->top + 1;
-find_key_array(a);
-while (cc <= 4)
-{
-    i = 0;
-    key_storage = check_phase(a, cc);
-    while(i <= (l / 4) && a->top != -1)
+    int i = 0;
+    while (i < a->top)
     {
-        c = 0;
-        a->full_len = a->top + 1;
-        printf("Key:%i:%i\n", cc, key_storage);
-        while (a->nbr[a->top - c] > key_storage && c != a->top - 1)
+        if (a->nbr[i] > a->nbr[i + 1])
         {
-            c++;
+            sa(a);
         }
-        printf("nbr: %i\natop:%i\nc%i\n", a->nbr[a->top - c], a->top, c);
-        smart_rotate(a, b, c);
-        pb(a, b);
+        ra(a);
         i++;
     }
-cc++;
 }
-}
-
-void  sort_b_to_a (Stack* a, Stack* b)
-{
-    int c;
-    while (b->top != -1)
-    {    
-        find_biggest_b(b);
-        b->full_len = b->top + 1;
-        c = 0;
-        while (b->nbr[b->top - c] < b->current_biggest && c != b->top)
-        {
-            c++;
-        }
-        printf("found nbr: %i\n", b->nbr[b->top - c]);
-        smart_rotate_b(b, c);
-        printf("Current biggest:%i\nProcessed nbr: %i\n", b->current_biggest, b->nbr[b->top]);
-        pa(a, b);
-        //nsmart_reverse_rotate(b, c);
-    }
-
-}
-
-
 
 void push_swap_beta(void)
 {
@@ -213,31 +151,29 @@ void push_swap_beta(void)
    fill_stack_a(&a);
    //constfil_a(&a);
 
-      print_stack(&a, "Stack a:\n");
+          print_stack(&a, "Stack a:\n");
    
-      print_stack(&b, "Stack b:\n");
+          print_stack(&b, "Stack b:\n");
    a.ar = stack_to_array(&a);
-      print_array(a.ar, a.top + 1);
+         print_array(a.ar, a.top + 1);
    
     sort_array(a.ar, a.top + 1);
-      print_array(a.ar, a.top + 1);
-
+          print_array(a.ar, a.top + 1);
     find_value_a(&a, &b);
 
-    print_stack(&a, "Stack a:\n");
-    print_stack(&b, "Stack b:\n");
+        print_stack(&a, "Stack a:\n");
+        print_stack(&b, "Stack b:\n");
 
     sort_b_to_a(&a, &b);
 
-    print_stack(&a, "Stack a:\n");
-    print_stack(&b, "Stack b:\n");
+        print_stack(&a, "Stack a:\n");
+        print_stack(&b, "Stack b:\n");
     fm = a.full_move_count + b.full_move_count;
-    printf("Moves:%i", fm);
+    int issort = is_sorted(&a);
+    printf("Moves:%i\nSort: %i", fm, issort);
 }
 
 int main(void)
 {
     push_swap_beta();
-
-
 }
