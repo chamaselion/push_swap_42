39ab4b046d3b964149981f0f9b2818c2e0140e34
chamaselion <chamaselion@gmail.com>
2024-05-14 15:14:32 +0200

    Created new file for the code handling larger sets of integers, optimized find_key_array function for mhundred to fit under 25 lines

--- sort_more_than_hundred.c
+++ sort_more_than_hundred.c
@@ -1,24 +1,46 @@
-void find_key_array(Stack* a)
+#include "push_swap.h"
+
+int check_phase_mhundred(Stack* a, int cc)
+{
+    if(cc <= 1)
+        return(a->current_mid1);
+    if(cc <= 2)
+        return(a->current_mid2);
+    if(cc <= 3)
+        return(a->current_mid3);
+    if(cc <= 4)
+        return(a->current_mid4);
+    if(cc <= 5)
+        return(a->current_mid5);
+    if(cc <= 6)
+        return(a->current_mid6);
+    if(cc <= 7)
+        return(a->current_mid7);
+    if(cc <= 8)
+	{
+        return(a->current_mid8);
+	}
+	return (0);
+}
+
+void find_key_array_mhundred(Stack* a)
 {
     int chunk_size;
-    int chunk1_end;
-    int chunk2_end;
-    int chunk3_end;
-    int chunk4_end;
      
-    chunk_size = (a->top + 1) / 4;
-    chunk1_end = chunk_size;
-    chunk2_end = chunk_size * 2;
-    chunk3_end = chunk_size * 3;
-    chunk4_end  = a->top;
-    a->current_mid1 = a->ar[chunk1_end];
-    a->current_mid2 = a->ar[chunk2_end];
-    a->current_mid3 = a->ar[chunk3_end];
-    a->current_mid4 = a->ar[chunk4_end];
+    chunk_size = (a->top + 1) / 8;
+
+    a->current_mid1 = a->ar[chunk_size];
+    a->current_mid2 = a->ar[chunk_size * 2];
+    a->current_mid3 = a->ar[chunk_size * 3];
+    a->current_mid4 = a->ar[chunk_size * 4];
+    a->current_mid5 = a->ar[chunk_size * 5];
+    a->current_mid6 = a->ar[chunk_size * 6];
+    a->current_mid7 = a->ar[chunk_size * 7];
+    a->current_mid8 = a->ar[a->top];
 
 }
 
-void find_value_a(Stack* a, Stack* b)
+void do_mhundred(Stack* a, Stack* b)
 {
     int c;
     int cc;
@@ -28,12 +50,12 @@ void find_value_a(Stack* a, Stack* b)
 
     cc = 1;
     l = a->top + 1;
-find_key_array(a);
-while (cc <= 4)
+find_key_array_mhundred(a);
+while (cc <= 8)
 {
     i = 0;
-    key_storage = check_phase(a, cc);
-    while(i <= (l / 4) && a->top != -1)
+    key_storage = check_phase_mhundred(a, cc);
+    while(i <= (l / 8) && a->top != -1)
     {
         c = 0;
         a->full_len = a->top + 1;
@@ -43,30 +65,10 @@ while (cc <= 4)
             c++;
         }
         printf("nbr: %i\natop:%i\nc%i\n", a->nbr[a->top - c], a->top, c);
-        smart_rotate(a, b, c);
+        smart_rotate(a, c);
         pb(a, b);
         i++;
     }
 cc++;
 }
-}
-
-void  sort_b_to_a (Stack* a, Stack* b)
-{
-    int c;
-    while (b->top != -1)
-    {    
-        find_biggest_b(b);
-        b->full_len = b->top + 1;
-        c = 0;
-        while (b->nbr[b->top - c] < b->current_biggest && c != b->top)
-        {
-            c++;
-        }
-        printf("found nbr: %i\n", b->nbr[b->top - c]);
-        smart_rotate_b(b, c);
-        printf("Current biggest:%i\nProcessed nbr: %i\n", b->current_biggest, b->nbr[b->top]);
-        pa(a, b);
-    }
-
 }
