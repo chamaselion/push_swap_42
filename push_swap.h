248bd6e52d904c91fec81d919efb0275ad2e8657
chamaselion <chamaselion@gmail.com>
2024-07-08 13:56:29 +0200

    changed for windows to test

--- push_swap.h
+++ push_swap.h
@@ -15,14 +15,9 @@ typedef struct {
     int full_len;
     int full_move_count;
     int* ar;
-    int current_mid1;
-    int current_mid2;
-    int current_mid3;
-    int current_mid4;
-    int current_mid5;
-    int current_mid6;
-    int current_mid7;
-    int current_mid8;
+    int* current_mid;
+	int no_chunks;
+	int	ind;
 } Stack;
 
 
@@ -38,7 +33,6 @@ void print_array(int* array, int size);
 int* stack_to_array(Stack* a);
 int check_phase(Stack* a, int cc);
 void smart_rotate(Stack* a, int c);
-void  nsmart_reverse_rotate(Stack* b, int c);
 void sort_array(int* array, int size);
 void print_stack(Stack* a, char* name);
 int find_smallest(Stack* a);
@@ -62,6 +56,21 @@ int check_phase_mhundred(Stack* a, int cc);
 void do_hundred(Stack* a, Stack* b);
 void put_sort(Stack* a, Stack* b);
 void put_sort_a_to_be(Stack* a, Stack* b, int c);
-void    let_it_be(Stack* a, Stack* b);
+void let_it_be(Stack* a, Stack* b);
+int check_phase_hundred_p(Stack* phantom_a, int cc);
+void do_hundred_p(Stack* phantom_a, Stack* phantom_b);
+void sort_b_to_a_p (Stack* phantom_a, Stack* phantom_b);
+void find_key_array_p(Stack* phantom_a);
+void sa_p(Stack *phantom_a);
+void sb_p(Stack *phantom_b);
+void ra_p(Stack* phantom_a);
+void rra_p(Stack* phantom_a);
+void rrb_p(Stack* phantom_b);
+void pb_p(Stack* phantom_a, Stack* phantom_b);
+void pa_p(Stack* phantom_a, Stack* phantom_b);
+void rb_p(Stack* phantom_b);
+int find_optimal_chunks(Stack* a, Stack* b);
+void smart_rotate_b_p(Stack* phantom_b, int c);
+void find_biggest_b_p(Stack* phantom_b);
 
 #endif
