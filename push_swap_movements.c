ac2ff222a15210dc60c26bbd968e7945dfd0b0f2
Balint Szikora <bszikora@1-E-22.42heilbronn.de>
2024-07-17 16:00:24 +0200

    justnorminetthings

--- push_swap_movements.c
+++ push_swap_movements.c
@@ -1,121 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_movements.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 14:15:03 by bszikora          #+#    #+#             */
+/*   Updated: 2024/07/16 15:16:03 by bszikora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-void sa(Stack *a)
+void	sa(t_Stack *a)
 {
-    if (a->top >= 1)
-    {
-        int temp = a->nbr[a->top];
-        a->nbr[a->top] = a->nbr[a->top - 1];
-        a->nbr[a->top - 1] = temp;
-    }
-    a->full_move_count++;
+	int	temp;
+
+	if (a->top >= 1)
+	{
+		temp = a->nbr[a->top];
+		a->nbr[a->top] = a->nbr[a->top - 1];
+		a->nbr[a->top - 1] = temp;
+	}
+	a->full_move_count++;
 }
 
-void sb(Stack *b)
+void	sb(t_Stack *b)
 {
-    if (b->top >= 1)
-    {
-        int temp = b->nbr[b->top];
-        b->nbr[b->top] = b->nbr[b->top - 1];
-        b->nbr[b->top - 1] = temp;
-    }
-    b->full_move_count++;
+	int	temp;
+
+	if (b->top >= 1)
+	{
+		temp = b->nbr[b->top];
+		b->nbr[b->top] = b->nbr[b->top - 1];
+		b->nbr[b->top - 1] = temp;
+	}
+	b->full_move_count++;
 }
 
-void ra(Stack* a)
+void	ra(t_Stack *a)
 {
-    if (a->top < 0)
-    {
-        return;
-    }
+	int	temp;
+	int	i;
 
-    int temp = a->nbr[a->top];
-
-    for (int i = a->top; i > 0; i--)
-    {
-        a->nbr[i] = a->nbr[i - 1];
-    }
-
-    a->nbr[0] = temp;
-    printf("ra\n");
-    a->full_move_count++;
+	i = a->top;
+	if (a->top < 0)
+	{
+		return ;
+	}
+	temp = a->nbr[a->top];
+	while (i > 0)
+	{
+		a->nbr[i] = a->nbr[i - 1];
+		i--;
+	}
+	a->nbr[0] = temp;
+	printf("ra\n");
+	a->full_move_count++;
 }
 
-void rb(Stack* b)
+void	rb(t_Stack *b)
 {
-    if (b->top < 0)
-    {
-        return;
-    }
+	int	temp;
+	int	i;
 
-    int temp = b->nbr[b->top];
-
-    for (int i = b->top; i > 0; i--)
-    {
-        b->nbr[i] = b->nbr[i - 1];
-    }
-
-    b->nbr[0] = temp;
-    printf("rb\n");
-    b->full_move_count++;
+	i = b->top;
+	if (b->top < 0)
+	{
+		return ;
+	}
+	temp = b->nbr[b->top];
+	while (i > 0)
+	{
+		b->nbr[i] = b->nbr[i - 1];
+		i--;
+	}
+	b->nbr[0] = temp;
+	printf("rb\n");
+	b->full_move_count++;
 }
 
-void pb(Stack* a, Stack* b)
+void	pb(t_Stack *a, t_Stack *b)
 {
-    if (a->top >= 0)
-    {
-        b->nbr[++b->top] = a->nbr[a->top--];
-    }
-    printf("pb\n");
-    a->full_move_count++;
-}
-
-void pa(Stack* a, Stack* b)
-{
-    if (b->top >= 0)
-    {
-        a->nbr[++a->top] = b->nbr[b->top--];
-    }
-    printf("pa\n");
-    a->full_move_count++;
-}
-
-void rra(Stack* a)
-{
-    if (a->top < 0)
-    {
-        return;
-    }
-
-    int temp = a->nbr[0];
-
-
-    for (int i = 0; i < a->top; i++)
-    {
-        a->nbr[i] = a->nbr[i + 1];
-    }
-
-    a->nbr[a->top] = temp;
-    printf("rra\n");
-    a->full_move_count++;
-}
-
-void rrb(Stack* b)
-{
-    if (b->top < 0)
-    {
-        return;
-    }
-
-    int temp = b->nbr[0];
-
-
-    for (int i = 0; i < b->top; i++)
-    {
-        b->nbr[i] = b->nbr[i + 1];
-    }
-
-    b->nbr[b->top] = temp;
-    printf("rrb\n");
-    b->full_move_count++;
+	if (a->top >= 0)
+	{
+		b->nbr[++b->top] = a->nbr[a->top--];
+	}
+	printf("pb\n");
+	a->full_move_count++;
 }
