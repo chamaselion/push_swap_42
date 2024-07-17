e621d785d9f446401d7bf36b80a0f3eceef6f166
Balint Szikora <bszikora@1-E-22.42heilbronn.de>
2024-07-17 16:02:46 +0200

    justnorminetthings

--- push_swap_stack_utis.c
+++ push_swap_stack_utis.c
@@ -1,171 +1,133 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_stack_utis.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 14:15:17 by bszikora          #+#    #+#             */
+/*   Updated: 2024/07/16 16:22:51 by bszikora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-void smart_rotate(Stack* a, int c)
+void	rotate_a_down(t_Stack *a, int c)
 {
-    int move_count;
+	int	move_count;
 
-    move_count = 0;
-
-if (a->full_len % 2 == 0)
-{
-
-    if (c >= a->full_len / 2)
-    {
-        while (move_count < ((a->top + 1) - c))
-        {
-            rra(a);
-            move_count++;
-        }
-    } else {
-        while (move_count < c)
-        {
-            ra(a);
-            move_count++;
-        }
-    }
-}
-else
-{
-    if (c > (a->full_len) / 2)
-    {
-        while (move_count < ((a->top + 1) - c))
-        {
-            rra(a);
-            move_count++;
-        }
-    }
-    else
-    {
-        while (move_count < c)
-        {
-            ra(a);
-            move_count++;
-        }
-    }
-}
+	move_count = 0;
+	while (move_count < ((a->top + 1) - c))
+	{
+		rra(a);
+		move_count++;
+	}
 }
 
-void smart_rotate_b(Stack* b, int c)
+void	rotate_a_up(t_Stack *a, int c)
 {
-    int move_count;
+	int	move_count;
 
-    move_count = 0;
-    
-if (b->full_len % 2 == 0)
-{
-
-    if (c > b->full_len / 2)
-    {
-        while (move_count < ((b->top + 1) - c))
-        {
-            rrb(b);
-            move_count++;
-        }
-    }
-    else
-    {
-        while (move_count < c)
-        {
-            rb(b);
-            move_count++;
-        }
-    }
-}
-else
-{
-    if (c > (b->full_len) / 2)
-    {
-        while (move_count < ((b->top + 1) - c))
-        {
-            rrb(b);
-            move_count++;
-        }
-    }
-    else
-    {
-        while (move_count < c)
-        {
-            rb(b);
-            move_count++;
-        }
-    }
-}
+	move_count = 0;
+	while (move_count < c)
+	{
+		ra(a);
+		move_count++;
+	}
 }
 
-void print_stack(Stack* a, char* name)
+void	smart_rotate(t_Stack *a, int c)
 {
-    printf("%s: ", name);
-    for (int i = a->top; i >= 0; i--)
-    {
-        printf("%d ", a->nbr[i]);
-    }
-    printf("\n");
+	if (a->full_len % 2 == 0)
+	{
+		if (c >= a->full_len / 2)
+		{
+			rotate_a_down(a, c);
+		}
+		else
+		{
+			rotate_a_up(a, c);
+		}
+	}
+	else
+	{
+		if (c > (a->full_len) / 2)
+		{
+			rotate_a_down(a, c);
+		}
+		else
+		{
+			rotate_a_up(a, c);
+		}
+	}
+}
+/*
+void	smart_rotate(t_Stack *a, int c)
+{
+	int	move_count;
+
+	move_count = 0;
+	if (a->full_len % 2 == 0)
+	{
+		if (c >= a->full_len / 2)
+		{
+			while (move_count < ((a->top + 1) - c))
+			{
+				rra(a);
+				move_count++;
+			}
+		}
+		else
+		{
+			while (move_count < c)
+			{
+				ra(a);
+				move_count++;
+			}
+		}
+	}
+	else
+	{
+		if (c > (a->full_len) / 2)
+		{
+			while (move_count < ((a->top + 1) - c))
+			{
+				rra(a);
+				move_count++;
+			}
+		}
+		else
+		{
+			while (move_count < c)
+			{
+				ra(a);
+				move_count++;
+			}
+		}
+	}
+}
+*/
+
+void	rotate_b_down(t_Stack *b, int c)
+{
+	int	move_count;
+
+	move_count = 0;
+	while (move_count < ((b->top + 1) - c))
+	{
+		rrb(b);
+		move_count++;
+	}
 }
 
-int find_smallest(Stack* a)
+void	rotate_b_up(t_Stack *b, int c)
 {
-    int smallest = a->nbr[0];
-    int i;
+	int	move_count;
 
-    i = 1;
-    while (i <= a->top)
-    {
-        if (a->nbr[i] < smallest)
-        {
-            smallest = a->nbr[i];
-        }
-        i++;
-    }
-    return smallest;
-}
-
-int find_smallest_b(Stack* b)
-{
-    int smallest = b->nbr[0];
-    int i;
-
-    i = 1;
-    while (i <= b->top)
-    {
-        if (b->nbr[i] < smallest)
-        {
-            smallest = b->nbr[i];
-        }
-    i++;
-    }
-    return smallest;
-}
-
-int find_biggest(Stack* a)
-{
-    int biggest = a->nbr[0];
-    int i;
-
-    i = 1;
-    while (i <= a->top)
-    {
-        if (a->nbr[i] > biggest)
-        {
-            biggest = a->nbr[i];
-        }
-        i++;
-    }
-    return biggest;
-}
-
-void find_biggest_b(Stack* b)
-{
-    int biggest = b->nbr[0];
-    int i;
-
-    i = 1;
-    while (i <= b->top)
-    {
-        if (b->nbr[i] > biggest)
-        {
-            biggest = b->nbr[i];
-        }
-        i++;
-    }
-    b->current_biggest = biggest;
+	move_count = 0;
+	while (move_count < c)
+	{
+		rb(b);
+		move_count++;
+	}
 }
