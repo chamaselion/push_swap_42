bc14ca823589a408d50bb9df03d8aae2f1108136
chamaselion <chamaselion@gmail.com>
2024-07-23 19:03:02 +0200

    Added precheck function for only one argument

--- push_swap_error_stuff2.c
+++ push_swap_error_stuff2.c
@@ -12,35 +12,71 @@
 
 #include "push_swap.h"
 
-void	check_pre_stack_single(int argc, char *argv)
-{
-	int	*check_array;
-	int	i;
-	int	val;
+#include <ctype.h>
+#include <stdlib.h>
 
-	check_array = malloc((argc - 1) * sizeof(int)); 
-	i = 1;
-	if (argv[1] == '\0' || argv[0] == '\0')
+int getNextInt(char *str, char **nextPos) {
+    if (str == NULL || *str == '\0') {
+        *nextPos = str;
+        return 0;
+    }
+    while (*str && !isdigit(*str) && *str != '-' && *str != '+') {
+        str++;
+    }
+
+    if (*str == '\0') {
+        *nextPos = str;
+        return 0;
+    }
+    char *end;
+    int num = (int)strtol(str, &end, 10);
+    *nextPos = end;
+
+    return num;
+}
+
+int	is_it_duplicate_tho(int *array, int limit)
+{
+    int i;
+    int j;
+
+	i = 0;
+
+    while (i < limit - 1)
+	{
+        j = i + 1;
+        while (j < limit)
+		{
+            if (array[i] == array[j])
+			{
+                return 1;
+            }
+            j++;
+        }
+        i++;
+    }
+    return 0;
+}
+
+void check_pre_stack_single(char *argv)
+{
+	int c;
+
+	c = 0;
+	printf("The string:%s\n", argv);
+	if (argv[0] == '\0')
 	{
 		write_error("Error\n");
 		exit(EXIT_FAILURE);
 	}
-	while (argv[i])
-	{
-		if (!is_integer(argv))
+	while (argv[c] != '\0')
+	{   
+		if (!((argv[c] >= '0' && argv[c] <= '9') || argv[c] == ' ' || argv[c] == '\t'))
 		{
 			write_error("Error\n");
 			exit(EXIT_FAILURE);
 		}
-		val = atoi(argv);
-		if (is_value_duplicate(check_array, val, strlen(argv)))
-		{
-			write_error("Error\n");
-			exit(EXIT_FAILURE);
-		}
-		check_array[i] = val;
-		i++;
+		c++;
 	}
-	free(check_array);
 }
 
